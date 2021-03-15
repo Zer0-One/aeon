9a08837383fa1f5b3e34f8298126d415e1e8c155
David Zero <zero-one@zer0-one.net>
2021-03-15 02:23:26 -0700

    Add test GUI and build system.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # aeon
+
 Experimental frontend for Hastur
+
+```
+“That is not dead which can eternal lie,
+And with strange aeons even death may die.”
+```
+
+## Dependencies
+
+gtk4
+
+## Building
+
+To build and install, run:
+
+```
+cmake . && make && make install
+```
+
+By default, builds are installed into /usr/local. To change this prefix, invoke
+cmake with the desired path. For example:
+
+```
+cmake . -DCMAKE_INSTALL_PREFIX=/
+```
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <gtk/gtk.h>
+
+static void activate(GtkApplication* app, gpointer user_data){
+    GtkWidget* window = gtk_application_window_new(app);
+
+    gtk_window_set_title(GTK_WINDOW(window), "Muh Test Window");
+    gtk_window_set_default_size(GTK_WINDOW(window), 500, 500);
+    gtk_widget_show(window);
+}
+
+int main(int argc, char** argv){
+    GtkApplication* app = gtk_application_new("org.myhead.asplode", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    int status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+
+    return status;
+}
